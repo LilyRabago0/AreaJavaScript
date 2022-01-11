4e94ede3aedd3c93a635f6c8112e203c1af29821
LilyRabago0 <lilyrabago01@gmail.com>
2022-01-10 19:25:51 -0600

    Add files via upload
    Actividades del día 10 de Enero. 
    -Introducción Javascript.
    -Ejercicios en equipo "EjerciciosFelipe"

---
+++ script.js
@@ -0,0 +1,72 @@
+/*var nombre = "Lilia Abigail";
+
+var apellidos = "Rabago Álvarez";
+
+var edad = 27;
+
+console.log(nombre, apellidos, edad);
+
+alert("Mi nombre es: " + nombre);
+
+console.log("Mis apellidos son: " + apellidos);
+
+alert("Mi nombre es: "+ nombre + ", mis apellidos son: " + apellidos + " y mi edad es: " +edad);
+
+//Otro ejemplo de como mostrar variabls en consola
+
+var numero1 = 5;
+var numero2 = 7;
+var resultado = numero1 + numero2;
+
+console.log("El resultado de la suma es: " + resultado);
+
+
+
+//Declaración de función 
+
+function Saludar(){
+
+    alert("Hola a todos");
+
+}
+
+//Expresión de función 
+
+var Cantar = function(){
+
+alert("A mi me gusta cantar");
+
+}
+Cantar();
+
+//Función sin parametros
+
+function sumar(){
+    var numero1 = 5;
+    var numero2 = 6;
+    var suma = numero1 + numero2;
+
+    alert(" La suma de la función sin parametros es: "  + suma);
+
+}
+
+sumar();
+
+//Función con parametros
+function restar(num1, num2, resta){
+    var num1 = 6;
+    var num2 = 8;
+    var resta = num1 - num2;
+    alert ("La resta de la función con parametros es: " + resta);
+}
+
+restar();
+
+//otra funcion con parametros
+
+function Cuadrado (cuadrado){
+    var
+}
+
+
+
